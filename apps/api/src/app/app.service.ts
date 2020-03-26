0205df02be3be71f5df85d4508f1fae68f0802b9
Tom Lewis <tlewismedia@gmail.com>
2020-03-26 14:21:52 -0700

    add more books descriptions

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -9,13 +9,31 @@ export class AppService {
       title: 'Title1',
       author: 'Author1',
       rating: 5,
-      desc: 'Lorem Ipsum'
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis massa tincidunt dui ut ornare lectus sit amet est. Sit amet tellus cras adipiscing enim eu. Cras tincidunt lobortis feugiat vivamus. '
     }, 
     { 
       title: 'Title2',
       author: 'Author2',
       rating: 4,
-      desc: 'dolor sit amet'
+      desc: 'Eu sem integer vitae justo eget magna fermentum iaculis eu. Mauris rhoncus aenean vel elit scelerisque mauris. Amet nisl suscipit adipiscing bibendum est ultricies integer quis.'
+    },
+    { 
+      title: 'Title3',
+      author: 'Author3',
+      rating: 2,
+      desc: 'Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Quis risus sed vulputate odio ut enim. '
+    },
+    { 
+      title: 'Title4',
+      author: 'Author4',
+      rating: 1,
+      desc: 'Nam aliquam sem et tortor. Aliquet lectus proin nibh nisl condimentum. Dui vivamus arcu felis bibendum ut tristique et. Est sit amet facilisis magna etiam.'
+    },
+    { 
+      title: 'Title5',
+      author: 'Author5',
+      rating: 4,
+      desc: 'Elementum tempus egestas sed sed. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices. '
     }
   ];
 
